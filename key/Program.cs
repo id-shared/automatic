c7c2ad7f41baeb7444e507567d52406135a1fa3c
id-shared <it.is.a.shared@gmail.com>
2024-09-08 16:17:57 -0700

    changes

--- key/Program.cs
+++ key/Program.cs
@@ -7,18 +7,12 @@ class Program {
   private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
 
   private static readonly LowLevelKeyboardProc _keyboardProc = KeyboardHookCallback;
-  private static readonly LowLevelMouseProc _mouseProc = MouseHookCallback;
   private static IntPtr _keyboardHookID = IntPtr.Zero;
   private static IntPtr _mouseHookID = IntPtr.Zero;
   private static readonly ConcurrentDictionary<string, bool> _keyStates = new();
 
   static async Task Main() {
     _keyboardHookID = await Task.Run(() => SetHook(_keyboardProc, WH_KEYBOARD_LL));
-    _mouseHookID = await Task.Run(() => SetHook(_mouseProc, WH_MOUSE_LL));
-
-    if (_keyboardHookID == IntPtr.Zero || _mouseHookID == IntPtr.Zero) {
-      return;
-    }
 
     Thread messageLoopThreads = new Thread(MonitorKeyStates);
     messageLoopThreads.IsBackground = true;
@@ -78,20 +72,6 @@ class Program {
       }
     }
     return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
-  }
-
-  private static IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
-    if (nCode >= 0) {
-      switch ((int)wParam) {
-        case WM_LBUTTONDOWN:
-          _keyStates["LMB"] = true;
-          break;
-        case WM_LBUTTONUP:
-          _keyStates["LMB"] = false;
-          break;
-      }
-    }
-    return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
   }
 
   [StructLayout(LayoutKind.Sequential)]
