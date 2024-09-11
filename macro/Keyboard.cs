423b6cf9d1403b45ae79208dcc1dfc5e25784305
id-shared <it.is.a.shared@gmail.com>
2024-09-11 05:21:15 -0700

    changes

---
+++ macro/Keyboard.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.InteropServices;
+
+class Keyboard {
+  public static uint I(uint key, bool is_pressed) {
+    INPUT[] inputs = new INPUT[1];
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].mkhi.ki = new KEYBDINPUT {
+      wVk = (ushort)key,
+      wScan = 0,
+      dwFlags = is_pressed ? 0 : KEYEVENTF_KEYUP,
+      time = 0,
+      dwExtraInfo = IntPtr.Zero
+    };
+
+    return SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+  }
+
+  const uint INPUT_KEYBOARD = 1;
+  const uint KEYEVENTF_KEYUP = 0x0002;
+
+  [StructLayout(LayoutKind.Sequential)]
+  struct INPUT {
+    public uint type;
+    public MOUSEKEYBDHARDWAREINPUT mkhi;
+  }
+
+  [StructLayout(LayoutKind.Explicit)]
+  struct MOUSEKEYBDHARDWAREINPUT {
+    [FieldOffset(0)]
+    public MOUSEINPUT mi;
+    [FieldOffset(0)]
+    public KEYBDINPUT ki;
+    [FieldOffset(0)]
+    public HARDWAREINPUT hi;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  struct KEYBDINPUT {
+    public ushort wVk;
+    public ushort wScan;
+    public uint dwFlags;
+    public uint time;
+    public IntPtr dwExtraInfo;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  struct MOUSEINPUT {
+    public int dx;
+    public int dy;
+    public uint mouseData;
+    public uint dwFlags;
+    public uint time;
+    public IntPtr dwExtraInfo;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  struct HARDWAREINPUT {
+    public uint uMsg;
+    public ushort wParamL;
+    public ushort wParamH;
+  }
+
+  [DllImport("user32.dll")]
+  static extern uint SendInput(uint nInputs, [In] INPUT[] pInputs, int cbSize);
+
+  [DllImport("user32.dll")]
+  static extern uint MapVirtualKey(uint uCode, uint uMapType);
+}
--- macro/Program.cs
+++ macro/Program.cs
@@ -4,17 +4,34 @@ using System.Collections.Concurrent;
 
 class Program {
   private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+  private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
   private static readonly LowLevelKeyboardProc d1_hook = D1HookCallback;
-  static ConcurrentDictionary<uint, bool> data = [];
+  private static readonly LowLevelMouseProc d2_hook = D2HookCallback;
+
+  static ConcurrentDictionary<uint, bool> data = new ConcurrentDictionary<uint, bool>();
   static IntPtr d1_hook_id = IntPtr.Zero;
+  static IntPtr d2_hook_id = IntPtr.Zero;
   static readonly bool F = false;
   static readonly bool T = true;
 
-  static async Task<bool> OnDown(uint key) {
+  static async Task<bool> OnD2Down() {
+    // Handle mouse down event here
+    Console.WriteLine("Mouse button pressed.");
+    return T;
+  }
+
+  static async Task<bool> OnD2Up() {
+    // Handle mouse up event here
+    Console.WriteLine("Mouse button released.");
+    return T;
+  }
+
+  static async Task<bool> OnD1Down(uint key) {
     return F;
   }
 
-  static async Task<bool> OnUp(uint key) {
+  static async Task<bool> OnD1Up(uint key) {
     switch (T) {
       case var _ when key.Equals((uint)ConsoleKey.A):
         return await Move((uint)ConsoleKey.RightArrow);
@@ -47,7 +64,9 @@ class Program {
   }
 
   static void Detach(nint id) {
-    UnhookWindowsHookEx(id);
+    if (id != IntPtr.Zero) {
+      UnhookWindowsHookEx(id);
+    }
   }
 
   static IntPtr SetHook(Delegate proc, int hookType) {
@@ -65,40 +84,73 @@ class Program {
   }
 
   static IntPtr D1HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
-    switch (T) {
-      case var _ when nCode >= 0:
-        uint key = (uint)Marshal.ReadInt32(lParam);
-        int act = (int)wParam;
-        switch (T) {
-          case var _ when act.Equals(WM_SYSKEYDOWN) || act.Equals(WM_KEYDOWN):
-            Task.Run(() => OnDown(key));
-            return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
-          case var _ when act.Equals(WM_SYSKEYUP) || act.Equals(WM_KEYUP):
-            Task.Run(() => OnUp(key));
-            return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
-          default:
-            return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
-        }
-      default:
-        return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
+    if (nCode >= 0) {
+      uint key = (uint)Marshal.ReadInt32(lParam);
+      int act = (int)wParam;
+      switch (act) {
+        case WM_SYSKEYDOWN:
+        case WM_KEYDOWN:
+          Task.Run(() => OnD1Down(key));
+          return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
+        case WM_SYSKEYUP:
+        case WM_KEYUP:
+          Task.Run(() => OnD1Up(key));
+          return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
+        default:
+          return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
+      }
     }
+    return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
+  }
+
+  static IntPtr D2HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+    if (nCode >= 0) {
+      int act = (int)wParam;
+      switch (act) {
+        case WM_LBUTTONDOWN:
+        case WM_LBUTTONUP:
+          Console.WriteLine("Left mouse button up.");
+          Task.Run(() => OnD2Up());
+          return CallNextHookEx(d2_hook_id, nCode, wParam, lParam);
+        case WM_RBUTTONDOWN:
+        case WM_RBUTTONUP:
+          Console.WriteLine("Right mouse button up.");
+          Task.Run(() => OnD2Up());
+          return CallNextHookEx(d2_hook_id, nCode, wParam, lParam);
+        default:
+          return CallNextHookEx(d1_hook_id, nCode, wParam, lParam);
+      }
+    }
+    return CallNextHookEx(d2_hook_id, nCode, wParam, lParam);
   }
 
   static void Main() {
     d1_hook_id = SetHook(d1_hook, WH_KEYBOARD_LL);
+    d2_hook_id = SetHook(d2_hook, WH_MOUSE_LL);
+
+    if (d2_hook_id == IntPtr.Zero) {
+      Console.WriteLine("Failed to set mouse hook.");
+    } else {
+      Console.WriteLine("Mouse hook set successfully.");
+    }
 
     Subscribe(new MSG());
 
     Detach(d1_hook_id);
+    Detach(d2_hook_id);
   }
 
   private const int WH_KEYBOARD_LL = 13;
+  private const int WH_MOUSE_LL = 14;
   private const int WM_KEYDOWN = 0x0100;
   private const int WM_SYSKEYDOWN = 0x0104;
   private const int WM_KEYUP = 0x0101;
   private const int WM_SYSKEYUP = 0x0105;
+  private const int WM_LBUTTONDOWN = 0x0201;
+  private const int WM_LBUTTONUP = 0x0202;
+  private const int WM_RBUTTONDOWN = 0x0204;
+  private const int WM_RBUTTONUP = 0x0205;
   private const int KEYEVENTF_KEYUP = 0x0002;
-  private const int INPUT_KEYBOARD = 1;
 
   [StructLayout(LayoutKind.Sequential)]
   private struct MSG {
@@ -140,95 +192,3 @@ class Program {
   [DllImport("user32.dll")]
   private static extern IntPtr DispatchMessage(ref MSG lpMsg);
 }
-
-class Keyboard {
-  public static uint I(uint key, bool is_pressed) {
-    INPUT[] inputs = new INPUT[1];
-
-    inputs[0].type = INPUT_KEYBOARD;
-    inputs[0].mkhi.ki = new KEYBDINPUT {
-      wVk = (ushort)key,
-      wScan = 0,
-      dwFlags = is_pressed ? 0 : KEYEVENTF_KEYUP,
-      time = 0,
-      dwExtraInfo = IntPtr.Zero
-    };
-
-    return SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
-  }
-
-  const uint INPUT_KEYBOARD = 1;
-  const uint KEYEVENTF_KEYUP = 0x0002;
-
-  [StructLayout(LayoutKind.Sequential)]
-  struct INPUT {
-    public uint type;
-    public MOUSEKEYBDHARDWAREINPUT mkhi;
-  }
-
-  [StructLayout(LayoutKind.Explicit)]
-  struct MOUSEKEYBDHARDWAREINPUT {
-    [FieldOffset(0)]
-    public MOUSEINPUT mi;
-    [FieldOffset(0)]
-    public KEYBDINPUT ki;
-    [FieldOffset(0)]
-    public HARDWAREINPUT hi;
-  }
-
-  [StructLayout(LayoutKind.Sequential)]
-  struct KEYBDINPUT {
-    public ushort wVk;
-    public ushort wScan;
-    public uint dwFlags;
-    public uint time;
-    public IntPtr dwExtraInfo;
-  }
-
-  [StructLayout(LayoutKind.Sequential)]
-  struct MOUSEINPUT {
-    public int dx;
-    public int dy;
-    public uint mouseData;
-    public uint dwFlags;
-    public uint time;
-    public IntPtr dwExtraInfo;
-  }
-
-  [StructLayout(LayoutKind.Sequential)]
-  struct HARDWAREINPUT {
-    public uint uMsg;
-    public ushort wParamL;
-    public ushort wParamH;
-  }
-
-  [DllImport("user32.dll")]
-  static extern uint SendInput(uint nInputs, [In] INPUT[] pInputs, int cbSize);
-
-  [DllImport("user32.dll")]
-  static extern uint MapVirtualKey(uint uCode, uint uMapType);
-}
-
-//static bool Stop(ConcurrentDictionary<uint, bool> dict, uint key_3, uint key_2, uint key_1, uint key, bool is_pressed) {
-//  switch (T) {
-//    case var _ when dict.GetOrAdd(key_3, F) && key_2.Equals(key):
-//      return Keyboard.SendKey(key_1, is_pressed);
-//    case var _ when key_3.Equals(key) && dict.GetOrAdd(key_2, F):
-//      return Keyboard.SendKey(key_1, is_pressed);
-//    case var _ when key_3.Equals(key):
-//      return Keyboard.SendKey(key_1, F);
-//    default:
-//      return F;
-//  }
-//}
-
-//Console.WriteLine("Scanning for all ConsoleKey values based on scan codes...");
-//uint startScanCode = 0x43;
-//uint endScanCode = 0xFF;
-
-//for (uint scanCode = startScanCode; scanCode <= endScanCode; scanCode++) {
-//  Thread.Sleep(1000);
-//  Console.WriteLine($"{scanCode}:");
-//  Keyboard.SendKey((uint)ConsoleKey.C, T);
-//  Keyboard.SendKey((uint)ConsoleKey.C, F);
-//}
