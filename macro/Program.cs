600437f269a17cc1079d62ecc26f2215155ad2a1
id-shared <it.is.a.shared@gmail.com>
2024-09-12 10:46:47 -0700

    Update Program.cs

--- macro/Program.cs
+++ macro/Program.cs
@@ -80,7 +80,7 @@ class Program {
         });
 
         switch (T) {
-          case var _ when duration >= 50:
+          case var _ when duration >= (time * 5):
             Keyboard.I(162, T);
             await Task.Delay(time);
             return await Stop(key, time, duration + time);
