ff4a8fde68d546d48e5f1f5d3ca48c0c18ff091e
id-shared <it.is.a.shared@gmail.com>
2024-09-24 20:41:36 -0700

    Update Program.cs

--- macro/Program.cs
+++ macro/Program.cs
@@ -147,53 +147,13 @@ class Program {
     d2_hook_id = SetHook(d2_hook, WH_KEYBOARD_LL);
     d1_hook_id = SetHook(d1_hook, WH_MOUSE_LL);
 
-    SubscribeVoice();
-    Subscribe(new MSG());
+    SubscribeKey(new MSG());
 
     Detach(d2_hook_id);
     Detach(d1_hook_id);
   }
 
-  public static DateTime since = DateTime.MinValue;
-  public static bool is_v_held = F;
-
-  public static bool SubscribeVoice() {
-    var waveIn = new WaveInEvent {
-      WaveFormat = new WaveFormat(44100, 1),
-      BufferMilliseconds = 100
-    };
-
-    waveIn.DataAvailable += (sender, e) => {
-      float current = GetMaxVolume(e.Buffer, e.BytesRecorded);
-      double needed = 20 * Math.Log10(current);
-
-      if (needed > -40 && !is_v_held) {
-        since = DateTime.Now;
-        Keyboard.Emulate(Key.V, T);
-        is_v_held = true;
-      } else if (is_v_held && (DateTime.Now - since).TotalMilliseconds >= 2000) {
-        Keyboard.Emulate(Key.V, F);
-        is_v_held = false;
-      }
-    };
-
-    waveIn.StartRecording();
-    return T;
-  }
-
-  public static float GetMaxVolume(byte[] buffer, int bytesRecorded) {
-    float maxVolume = 0;
-    for (int i = 0; i < bytesRecorded; i += 2) {
-      short sample = BitConverter.ToInt16(buffer, i);
-      float sample32 = sample / 32768f;
-
-      if (sample32 < 0) { sample32 = -sample32; }
-      if (sample32 > maxVolume) { maxVolume = sample32; }
-    }
-    return maxVolume;
-  }
-
-  private static void Subscribe(MSG msg) {
+  private static void SubscribeKey(MSG msg) {
     while (GetMessage(out msg, IntPtr.Zero, 0, 0)) {
       TranslateMessage(ref msg);
       DispatchMessage(ref msg);
@@ -258,3 +218,42 @@ class Program {
   [DllImport("user32.dll")]
   private static extern IntPtr DispatchMessage(ref MSG lpMsg);
 }
+
+//public static DateTime since = DateTime.MinValue;
+//public static bool is_v_held = F;
+
+//public static bool SubscribeVoice() {
+//  var waveIn = new WaveInEvent {
+//    WaveFormat = new WaveFormat(44100, 1),
+//    BufferMilliseconds = 100
+//  };
+
+//  waveIn.DataAvailable += (sender, e) => {
+//    float current = HighestVolume(e.Buffer, e.BytesRecorded);
+//    double needed = 20 * Math.Log10(current);
+
+//    if (needed > -40 && !is_v_held) {
+//      since = DateTime.Now;
+//      Keyboard.Emulate(Key.V, T);
+//      is_v_held = true;
+//    } else if (is_v_held && (DateTime.Now - since).TotalMilliseconds >= 2000) {
+//      Keyboard.Emulate(Key.V, F);
+//      is_v_held = false;
+//    }
+//  };
+
+//  waveIn.StartRecording();
+//  return T;
+//}
+
+//public static float HighestVolume(byte[] buffer, int bytesRecorded) {
+//  float maxVolume = 0;
+//  for (int i = 0; i < bytesRecorded; i += 2) {
+//    short sample = BitConverter.ToInt16(buffer, i);
+//    float sample32 = sample / 32768f;
+
+//    if (sample32 < 0) { sample32 = -sample32; }
+//    if (sample32 > maxVolume) { maxVolume = sample32; }
+//  }
+//  return maxVolume;
+//}
