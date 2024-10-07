73d674ff019d98e5f753b04844cf9a47035d5cae
id-shared <it.is.a.shared@gmail.com>
2024-10-07 04:28:27 -0700

    Update DedicatedWorker.cs

--- macro/DedicatedWorker.cs
+++ macro/DedicatedWorker.cs
@@ -37,35 +37,35 @@
   private const bool T = true;
 }
 
-class LockFreeRingBuffer<Item> {
-  public bool TryDequeue(out Item item) {
+class LockFreeRingBuffer<Action> {
+  public bool TryDequeue(out Action z) {
     lock (_buffer) {
       if (_head == _tail) {
-        item = default!;
+        z = default!;
         return F;
       }
 
-      item = _buffer[_head];
+      z = _buffer[_head];
       _buffer[_head] = default!;
       _head = (_head + 1) % _buffer.Length;
       return T;
     }
   }
 
-  public void Enqueue(Item item) {
+  public void Enqueue(Action z) {
     lock (_buffer) {
-      _buffer[_tail] = item;
+      _buffer[_tail] = z;
       _tail = (_tail + 1) % _buffer.Length;
     }
   }
 
-  public LockFreeRingBuffer(int size) {
-    _buffer = new Item[size];
+  public LockFreeRingBuffer(int k) {
+    _buffer = new Action[k];
     _head = 0;
     _tail = 0;
   }
 
-  private readonly Item[] _buffer;
+  private readonly Action[] _buffer;
   private static int _head;
   private static int _tail;
   private const bool F = false;
