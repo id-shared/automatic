585d77bec7ba266d3cd73b45d7c282d783a0df3c
id-shared <it.is.a.shared@gmail.com>
2024-10-09 19:46:51 -0700

    Update DedicatedWorker.cs

--- macro/DedicatedWorker.cs
+++ macro/DedicatedWorker.cs
@@ -1,4 +1,6 @@
-﻿class DedicatedWorker {
+﻿using System.Runtime.InteropServices;
+
+class DedicatedWorker {
   public bool Enqueue(Action action) {
     return TaskQueue.Enqueue(action);
   }
@@ -29,37 +31,60 @@
 }
 
 class LockFreeRingBuffer<Action> {
-  public bool TryDequeue(out Action z) {
-    lock (_buffer) {
-      if (_head == _tail) {
-        z = default!;
-        return F;
-      }
+  private readonly Action[] _buffer;
+  private readonly int _bufferMask;
 
-      z = _buffer[_head];
-      _buffer[_head] = default!;
-      _head = (_head + 1) % _buffer.Length;
-      return T;
+  public LockFreeRingBuffer(int size) {
+    if ((size & (size - 1)) != 0) {
+      throw new ArgumentException("Size must be a power of 2");
     }
+
+    _buffer = new Action[size];
+    _bufferMask = size - 1;
+    _head = new PaddedInt();
+    _tail = new PaddedInt();
+    _head.Value = 0;
+    _tail.Value = 0;
   }
 
-  public bool Enqueue(Action z) {
-    lock (_buffer) {
-      _buffer[_tail] = z;
-      _tail = (_tail + 1) % _buffer.Length;
+  public bool TryDequeue(out Action z) {
+    int localHead = _head.Value;
+    if (localHead == Volatile.Read(ref _tail.Value)) {
+      z = default!;
+      return F;
     }
+
+    z = _buffer[localHead & _bufferMask];
+    _buffer[localHead & _bufferMask] = default!;
+
+    _head.Value = localHead + 1;
     return T;
   }
 
-  public LockFreeRingBuffer(int k) {
-    _buffer = new Action[k];
-    _head = 0;
-    _tail = 0;
+  public bool Enqueue(Action z) {
+    int localTail = _tail.Value;
+    int nextTail = (localTail + 1);
+
+    if (nextTail == Volatile.Read(ref _head.Value) + _buffer.Length) {
+      // Buffer is full, cannot enqueue
+      return F;
+    }
+
+    _buffer[localTail & _bufferMask] = z;
+
+    _tail.Value = nextTail;
+    return T;
   }
 
-  private readonly Action[] _buffer;
-  private static int _head;
-  private static int _tail;
+  [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Explicit, Size = 128)]
+  private struct PaddedInt {
+    [FieldOffset(64)]
+    public int Value;
+  }
+
+  private PaddedInt _head;
+  private PaddedInt _tail;
+
   private const bool F = false;
   private const bool T = true;
 }
