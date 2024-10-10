dc9ce56a702b6d8d5457cafae30b6ab0b47129e2
id-shared <it.is.a.shared@gmail.com>
2024-10-09 19:47:53 -0700

    Update DedicatedWorker.cs

--- macro/DedicatedWorker.cs
+++ macro/DedicatedWorker.cs
@@ -31,22 +31,6 @@ class DedicatedWorker {
 }
 
 class LockFreeRingBuffer<Action> {
-  private readonly Action[] _buffer;
-  private readonly int _bufferMask;
-
-  public LockFreeRingBuffer(int size) {
-    if ((size & (size - 1)) != 0) {
-      throw new ArgumentException("Size must be a power of 2");
-    }
-
-    _buffer = new Action[size];
-    _bufferMask = size - 1;
-    _head = new PaddedInt();
-    _tail = new PaddedInt();
-    _head.Value = 0;
-    _tail.Value = 0;
-  }
-
   public bool TryDequeue(out Action z) {
     int localHead = _head.Value;
     if (localHead == Volatile.Read(ref _tail.Value)) {
@@ -76,11 +60,27 @@ class LockFreeRingBuffer<Action> {
     return T;
   }
 
+  public LockFreeRingBuffer(int size) {
+    if ((size & (size - 1)) != 0) {
+      throw new ArgumentException("Size must be a power of 2");
+    }
+
+    _buffer = new Action[size];
+    _bufferMask = size - 1;
+    _head = new PaddedInt();
+    _tail = new PaddedInt();
+    _head.Value = 0;
+    _tail.Value = 0;
+  }
+
   [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Explicit, Size = 128)]
   private struct PaddedInt {
     [FieldOffset(64)]
     public int Value;
   }
+
+  private readonly Action[] _buffer;
+  private readonly int _bufferMask;
 
   private PaddedInt _head;
   private PaddedInt _tail;
