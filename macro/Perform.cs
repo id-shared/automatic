18221108557eefa7e5c37b70b50b7f49a1956919
id-shared <it.is.a.shared@gmail.com>
2024-10-13 13:22:42 -0700

    Update Perform.cs

--- macro/Perform.cs
+++ macro/Perform.cs
@@ -3,23 +3,24 @@ using System.Diagnostics;
 
 class Perform {
   private static readonly WorkerPool FX = new(16, 1024);
-  private static readonly uint[] KR = [KeyA.R];
-  private static readonly uint[] KL = [KeyA.L];
-  private static volatile bool RMBX = A.F;
+  private static readonly uint[] MA = [KeyE.A];
+  private static readonly uint[] AR = [KeyA.R];
+  private static readonly uint[] AL = [KeyA.L];
   private static volatile bool LMBX = A.F;
   private static volatile bool FREE = A.T;
   private const int IT = 99;
+  private const int IN = 9;
 
   private static bool KeyDU() {
     FREE = A.F;
-    IO(IT, KL);
+    IO(IT, AL);
     FREE = A.T;
     return A.T;
   }
 
   private static bool KeyAU() {
     FREE = A.F;
-    IO(IT, KR);
+    IO(IT, AR);
     FREE = A.T;
     return A.T;
   }
@@ -68,15 +69,14 @@ class Perform {
         LMBX = A.T;
         FX.TryEnqueue(() => {
           Till(() => FREE);
-          IO(IT, [KeyE.A]);
-          _ = LMBX && I([KeyE.C, KeyE.A]);
+          Till(() => {
+            IO(IN, MA);
+            return !LMBX;
+          });
         });
         return next;
       case WM_LBUTTONUP:
         LMBX = A.F;
-        FX.TryEnqueue(() => {
-          _ = LMBX || O([KeyE.C, KeyE.A]);
-        });
         return next;
       default:
         return next;
@@ -131,7 +131,7 @@ class Perform {
   private static volatile IntPtr hookX2 = IntPtr.Zero;
   private static volatile IntPtr hookX1 = IntPtr.Zero;
 
-  private const uint WM_LBUTTONDOWN = 0x0201, WM_LBUTTONUP = 0x0202, WM_RBUTTONDOWN = 0x0204, WM_RBUTTONUP = 0x0204;
+  private const uint WM_LBUTTONDOWN = 0x0201, WM_LBUTTONUP = 0x0202;
   private const uint WM_KEYUP = 0x0101, WM_SYSKEYUP = 0x0105;
   private const uint WH_KEYBOARD_LL = 13, WH_MOUSE_LL = 14;
 
