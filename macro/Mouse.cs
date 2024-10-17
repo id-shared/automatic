e22689c1195f84cf749e181ebc746bdabe432d7d
id-shared <it.is.a.shared@gmail.com>
2024-10-17 08:31:24 -0700

    Update Mouse.cs

--- macro/Mouse.cs
+++ macro/Mouse.cs
@@ -19,13 +19,23 @@ class Mouse {
 }
 
 class DD {
-  public pDD_btn btn;         //Mouse button 
-  public pDD_whl whl;         //Mouse wheel
-  public pDD_mov mov;      //Mouse move abs. 
-  public pDD_movR movR;  //Mouse move rel. 
-  public pDD_key key;         //Keyboard 
-  public pDD_str str;            //Input visible char
-  public pDD_todc todc;      //VK to ddcode
+  public pDD_btn btn;
+  public pDD_whl whl;
+  public pDD_mov mov;
+  public pDD_movR movR;
+  public pDD_key key;
+  public pDD_str str;
+  public pDD_todc todc;
+
+  public delegate int pDD_btn(int btn);
+  public delegate int pDD_whl(int whl);
+  public delegate int pDD_key(int ddcode, int flag);
+  public delegate int pDD_mov(int x, int y);
+  public delegate int pDD_movR(int dx, int dy);
+  public delegate int pDD_str(string str);
+  public delegate int pDD_todc(int vkcode);
+
+  private IntPtr m_hinst;
 
   ~DD() {
     if (!m_hinst.Equals(IntPtr.Zero)) {
@@ -47,44 +57,34 @@ class DD {
 
     ptr = GetProcAddress(hinst, "DD_btn");
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
-    btn = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_btn)) as pDD_btn;
+    btn = Marshal.GetDelegateForFunctionPointer<pDD_btn>(ptr);
 
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
     ptr = GetProcAddress(hinst, "DD_whl");
-    whl = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_whl)) as pDD_whl;
+    whl = Marshal.GetDelegateForFunctionPointer<pDD_whl>(ptr);
 
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
     ptr = GetProcAddress(hinst, "DD_mov");
-    mov = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_mov)) as pDD_mov;
+    mov = Marshal.GetDelegateForFunctionPointer<pDD_mov>(ptr);
 
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
     ptr = GetProcAddress(hinst, "DD_key");
-    key = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_key)) as pDD_key;
+    key = Marshal.GetDelegateForFunctionPointer<pDD_key>(ptr);
 
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
     ptr = GetProcAddress(hinst, "DD_movR");
-    movR = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_movR)) as pDD_movR;
+    movR = Marshal.GetDelegateForFunctionPointer<pDD_movR>(ptr);
 
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
     ptr = GetProcAddress(hinst, "DD_str");
-    str = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_str)) as pDD_str;
+    str = Marshal.GetDelegateForFunctionPointer<pDD_str>(ptr);
 
     if (ptr.Equals(IntPtr.Zero)) { return -1; }
     ptr = GetProcAddress(hinst, "DD_todc");
-    //todc = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_todc)) as pDD_todc;
+    todc = Marshal.GetDelegateForFunctionPointer<pDD_todc>(ptr);
 
     return 1;
   }
-
-  public delegate int pDD_btn(int btn);
-  public delegate int pDD_whl(int whl);
-  public delegate int pDD_key(int ddcode, int flag);
-  public delegate int pDD_mov(int x, int y);
-  public delegate int pDD_movR(int dx, int dy);
-  public delegate int pDD_str(string str);
-  public delegate int pDD_todc(int vkcode);
-
-  private IntPtr m_hinst;
 
   [DllImport("Kernel32")]
   private static extern IntPtr LoadLibrary(string dllfile);
@@ -93,7 +93,7 @@ class DD {
   private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
 
   [DllImport("kernel32.dll")]
-  public static extern bool FreeLibrary(IntPtr hModule);
+  private static extern bool FreeLibrary(IntPtr hModule);
 }
 
 enum KeyModifiers {
