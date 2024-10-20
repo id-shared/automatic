c84fd4d059ef2e40fa02448eab0cac928ba31be9
id-shared <it.is.a.shared@gmail.com>
2024-10-20 10:51:17 -0700

    Update MouseInjection.cs

--- x/MouseInjection.cs
+++ x/MouseInjection.cs
@@ -7,7 +7,7 @@ class MouseInjection {
 }
 
 class Native {
-  [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
   public static extern bool DeviceIoControl(
     SafeFileHandle hDevice,
     uint dwIoControlCode,
@@ -17,5 +17,27 @@ class Native {
     uint nOutBufferSize,
     out uint lpBytesReturned,
     IntPtr lpOverlapped
+  );
+
+  [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+  public static extern SafeFileHandle CreateFileW(
+    string lpFileName,
+    uint dwDesiredAccess,
+    uint dwShareMode,
+    IntPtr lpSecurityAttributes, // Use IntPtr.Zero if not needed
+    uint dwCreationDisposition,
+    uint dwFlagsAndAttributes,
+    IntPtr hTemplateFile // Use IntPtr.Zero if not needed
+  );
+
+  [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true)]
+  public static extern SafeFileHandle CreateFileA(
+    string lpFileName,
+    uint dwDesiredAccess,
+    uint dwShareMode,
+    IntPtr lpSecurityAttributes, // Use IntPtr.Zero if not needed
+    uint dwCreationDisposition,
+    uint dwFlagsAndAttributes,
+    IntPtr hTemplateFile // Use IntPtr.Zero if not needed
   );
 }
