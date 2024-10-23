2d85736f1f6c96e3e8be07e936c32506aca3c548
id-shared <it.is.a.shared@gmail.com>
2024-10-23 12:24:57 -0700

    changes

--- x/Device1.cs
+++ x/Device1.cs
@@ -17,11 +17,11 @@ class Device1 {
   }
 
   public bool E(ushort e) {
-    return Act(new BtnI() {
-      ProcessId = process,
-      ButtonFlags = e,
-      ButtonData = 0
-    }, code.BTN, A.T);
+    return e switch {
+      2 => Device2.Input([KeyM.L], A.F),
+      1 => Device2.Input([KeyM.L], A.T),
+      _ => Device2.Input([KeyM.L], A.T),
+    }; 
   }
 
   public bool Act<X>(X x, uint e, bool a) {
@@ -45,71 +45,4 @@ class Device1 {
   private readonly IOCode code = new();
   private readonly IntPtr process;
   private readonly Context context;
-}
-
-class IOCode {
-  public IOCode() {
-    CONTEXT = Code(CODE, 2600, DeviceMethod.MethodBuffered, FileAccess.FileAnyAccess);
-    PACKET = Code(CODE, 2870, DeviceMethod.MethodBuffered, FileAccess.FileAnyAccess);
-    MOVE = Code(CODE, 2851, DeviceMethod.MethodBuffered, FileAccess.FileAnyAccess);
-    BTN = Code(CODE, 2850, DeviceMethod.MethodBuffered, FileAccess.FileAnyAccess);
-  }
-
-  private static uint Code(uint deviceType, uint function, DeviceMethod method, FileAccess access) {
-    return ((deviceType << 16) | ((uint)access << 14) | (function << 2) | (uint)method);
-  }
-
-  public enum FileAccess {
-    FileAnyAccess = 0,
-    FileReadAccess = 1,
-    FileWriteAccess = 2
-  }
-
-  public enum DeviceMethod {
-    MethodBuffered = 0,
-    MethodInDirect = 1,
-    MethodOutDirect = 2,
-    MethodNeither = 3
-  }
-
-  public readonly uint CONTEXT;
-  public readonly uint BTN;
-  public readonly uint PACKET;
-  public readonly uint MOVE;
-  public readonly uint CODE = 48781u;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct MoveI {
-  public IntPtr ProcessId;
-  public ushort IndicatorFlags;
-  public int MovementX;
-  public int MovementY;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct BtnI {
-  public IntPtr ProcessId;
-  public ushort ButtonFlags;
-  public ushort ButtonData;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct ContextO {
-  public ContextButtonO ButtonDevice;
-  public ContextMoveO MovementDevice;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct ContextButtonO {
-  public ushort UnitId;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct ContextMoveO {
-  [MarshalAs(UnmanagedType.I1)]
-  public bool AbsoluteMovement;
-  [MarshalAs(UnmanagedType.I1)]
-  public bool VirtualDesktop;
-  public ushort UnitId;
 }
