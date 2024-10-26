43dbb81220b109ce45aa1cf7107f8b9d5d873220
id-shared <it.is.a.shared@gmail.com>
2024-10-26 10:15:57 -0700

    changes

--- _/Usb.hpp
+++ _/Usb.hpp
@@ -1,16 +1,9 @@
-#pragma comment(lib, "ntdll.lib")
 #pragma once
-#include <cstdint>
 #include <functional>
-#include <iostream>
-#include <mutex>
-#include <string>
-#include <vector>
 #include <windows.h>
-#include <winternl.h>
 
 namespace Usb {
-  LPCWSTR read(std::function<bool(std::wstring_view name)> z) {
+  LPCWSTR read(std::function<bool(LPCWSTR name)> z) {
     return L"";
   }
 }
