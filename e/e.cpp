41080de290a4d16bfe42361ceec74db2daacdc5e
id-shared <it.is.a.shared@gmail.com>
2024-10-29 13:22:49 -0700

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -6,59 +6,30 @@
 #include <vector>
 #include <windows.h>
 
-std::vector<COLORREF> capture(int e_1, int e) {
-  HDC hScreenDC = GetDC(NULL);
-  HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
-
-  HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, e_1, e);
-  SelectObject(hMemoryDC, hBitmap);
-
-  int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-  int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-  int x = (screenWidth - e_1) / 2;
-  int y = (screenHeight - e) / 2;
-
-  BitBlt(hMemoryDC, 0, 0, e_1, e, hScreenDC, x, y, SRCCOPY);
-
-  BITMAP bmp;
-  GetObject(hBitmap, sizeof(BITMAP), &bmp);
+std::vector<COLORREF> capture(HDC hScreenDC, HDC hMemoryDC, HBITMAP hBitmap, int e_1, int e) {
+  BitBlt(hMemoryDC, 0, 0, e_1, e, hScreenDC, (GetSystemMetrics(SM_CXSCREEN) - e_1) / 2,
+    (GetSystemMetrics(SM_CYSCREEN) - e) / 2, SRCCOPY);
 
   BITMAPINFO bi;
   bi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-  bi.bmiHeader.biWidth = bmp.bmWidth;
-  bi.bmiHeader.biHeight = bmp.bmHeight;
+  bi.bmiHeader.biWidth = e_1;
+  bi.bmiHeader.biHeight = -e;  // Top-down DIB for direct order
   bi.bmiHeader.biPlanes = 1;
   bi.bmiHeader.biBitCount = 32;
   bi.bmiHeader.biCompression = BI_RGB;
-  bi.bmiHeader.biSizeImage = 0;
-  bi.bmiHeader.biXPelsPerMeter = 0;
-  bi.bmiHeader.biYPelsPerMeter = 0;
-  bi.bmiHeader.biClrUsed = 0;
-  bi.bmiHeader.biClrImportant = 0;
 
   std::vector<COLORREF> pixelData(e_1 * e);
   GetDIBits(hMemoryDC, hBitmap, 0, e, pixelData.data(), &bi, DIB_RGB_COLORS);
 
-  DeleteObject(hBitmap);
-  DeleteDC(hMemoryDC);
-  ReleaseDC(NULL, hScreenDC);
-
   return pixelData;
 }
 
-bool IsPurpleDominated(COLORREF x, double e) {
+inline bool IsPurpleDominated(COLORREF x, double e) {
   BYTE green = GetGValue(x);
-  BYTE blue = GetBValue(x);
-  BYTE red = GetRValue(x);
+  if (green == 0) return (GetRValue(x) > 0 && GetBValue(x) > 0);
 
-  if (green == 0) {
-    return (red > 0 && blue > 0);
-  }
-
-  double ratio_red_green = static_cast<double>(red) / green;
-  double ratio_blue_green = static_cast<double>(blue) / green;
-
-  return (ratio_red_green > e && ratio_blue_green > e);
+  double ratio = (static_cast<double>(GetRValue(x)) + GetBValue(x)) / (2 * green);
+  return ratio > e;
 }
 
 int main() {
@@ -69,24 +40,25 @@ int main() {
 
   HANDLE driver = Device::driver(device);
 
-  while (true) {
-    const int width = 64;
-    const int height = 1;
-    const int delta = (width / 2);
+  const int width = 64, height = 4, delta = width / 2;
+  HDC hScreenDC = GetDC(NULL);
+  HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
+  HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
+  SelectObject(hMemoryDC, hBitmap);
 
-    std::vector<COLORREF> pixelData = capture(width, height);
-    bool breaker = true;
-    bool r[delta] = {};
-    bool l[delta] = {};
+  while (true) {
+    std::vector<COLORREF> pixelData = capture(hScreenDC, hMemoryDC, hBitmap, width, height);
+    bool r[delta] = {}, l[delta] = {};
     bool active = true;
 
-    for (int i = 0; i < height; ++i) {
+    for (int i = 0; i < width; ++i) {
       for (int j = 0; j < width; ++j) {
-        COLORREF color = pixelData[(i * width) + j];
-        bool result = IsPurpleDominated(color, 1.1);
+        bool result = IsPurpleDominated(pixelData[(i * width) + j], 1.2);
         j < delta ? l[j] = result : r[j - delta] = result;
       }
     }
+
+    std::reverse(l, l + delta);
 
     for (int i = 0; i < delta && active; ++i) {
       if (r[i]) {
@@ -95,25 +67,16 @@ int main() {
       }
     }
 
-    std::reverse(l, l + delta);
-
     for (int i = 0; i < delta && active; ++i) {
       if (l[i]) {
         Xyloid2::yx(driver, 0, (i + 1) * -1);
         active = false;
       }
     }
-
-    //printf("%d, %d, %d, %d\n", r[0], r[1], r[2], r[3]);
-    //Time::XO(100);
   }
 
+  DeleteObject(hBitmap);
+  DeleteDC(hMemoryDC);
+  ReleaseDC(NULL, hScreenDC);
   return 0;
 }
-
-//if (j >= delta) {
-  //Xyloid2::yx(driver, 0, (j - delta) * +1);
-//}
-//else {
-  //Xyloid2::yx(driver, 0, ((delta + 2) - j) * -1);
-//}
