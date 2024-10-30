516f5293640d6ca72f300cefb407219c3cd849a2
id-shared <it.is.a.shared@gmail.com>
2024-10-30 09:58:47 -0700

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -183,10 +183,8 @@ int main() {
       }
     }
 
-    int y2 = zIndex(y2_, n);
+    int y2 = aIndex(y2_, n);
     int y1 = zIndex(y1_, n);
-    int ys = +4;
-    int yf = +4;
 
     int x2 = zIndex(x2_, n);
     int x1 = zIndex(x1_, n);
@@ -196,12 +194,21 @@ int main() {
     //std::cout << y1_[0] << " | " << y1_[1] << " | " << y1_[2] << " | " << y1_[3] << std::endl;
     //std::cout << x1 << " | " << x2 << std::endl;
 
-    if (y1 >= 1) {
-      //std::cout << y1 << " | " << y2 << std::endl;
-      Xyloid2::yx(driver, y1 * -1, 0);
-    }
-    else {
-      //Xyloid2::yx(driver, y2 * yf * +1, 0);
+    if (y1 >= 1 || y2 >= 1) {
+      if (y1 >= 1 && y2 >= 1) {
+        if (y2 > y1) {
+          Xyloid2::yx(driver, ((y2 - y1) / 2) * +1, 0);
+        }
+        else {
+          Xyloid2::yx(driver, ((y1 - y2) / 2) * -1, 0);
+        }
+      }
+      else if (y2 >= 1) {
+        Xyloid2::yx(driver, y2 * +1, 0);
+      }
+      else {
+        Xyloid2::yx(driver, y1 * -1, 0);
+      }
     }
 
     /*if (x1 >= 1 && x2 >= 1) {
