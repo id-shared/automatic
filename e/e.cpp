549e5a64e4303a944d059591cc3b1dff6b4278e7
id-shared <it.is.a.shared@gmail.com>
2024-10-30 07:48:49 -0700

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -178,13 +178,22 @@ int main() {
       }
     }
 
-    int y2 = zIndex(y2_, n);
+    int y2 = aIndex(y2_, n);
     int y1 = zIndex(y1_, n);
+    int yf = +5;
+
     int x2 = zIndex(x2_, n);
     int x1 = zIndex(x1_, n);
     int xf = +5;
 
     //std::cout << xr << " | " << xl << std::endl;
+
+    /*if (y1 >= 1) {
+      Xyloid2::yx(driver, y1 * yf * -1, 0);
+    }
+    else {
+      Xyloid2::yx(driver, y2 * yf * +1, 0);
+    }*/
 
     if (x1 >= 1 && x2 >= 1) {
       if (x1 > x2) {
@@ -197,10 +206,10 @@ int main() {
     else {
       if (x1 >= 1 || x2 >= 1) {
         if (x1 >= 1) {
-          Xyloid2::yx(driver, 0, (x1 / 2) * xf * -1);
+          Xyloid2::yx(driver, 0, x1 * xf * -1);
         }
         else {
-          Xyloid2::yx(driver, 0, (x2 / 2) * xf * +1);
+          Xyloid2::yx(driver, 0, x2 * xf * +1);
         }
       }
     }
