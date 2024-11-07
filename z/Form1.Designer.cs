6f2bd365d4372c8b5f4158103bbcdaeb844adfdb
id-shared <it.is.a.shared@gmail.com>
2024-11-07 02:03:53 -0800

    changes

---
+++ z/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SoundView {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.timer = new System.Windows.Forms.Timer(this.components);
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.SuspendLayout();
+      // 
+      // timer
+      // 
+      this.timer.Enabled = true;
+      this.timer.Interval = 10;
+      this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(113, 67);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(40, 22);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Left";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(361, 67);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(47, 20);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "Right";
+      // 
+      // panel1
+      // 
+      this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+      this.panel1.BackColor = System.Drawing.Color.Black;
+      this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.panel1.Location = new System.Drawing.Point(11, 52);
+      this.panel1.Margin = new System.Windows.Forms.Padding(2);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(38, 184);
+      this.panel1.TabIndex = 6;
+      // 
+      // panel2
+      // 
+      this.panel2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+      this.panel2.BackColor = System.Drawing.Color.Black;
+      this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.panel2.Location = new System.Drawing.Point(484, 52);
+      this.panel2.Margin = new System.Windows.Forms.Padding(2);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(38, 184);
+      this.panel2.TabIndex = 7;
+      // 
+      // Form1
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+      this.BackColor = System.Drawing.Color.Black;
+      this.ClientSize = new System.Drawing.Size(533, 292);
+      this.Controls.Add(this.panel2);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.DoubleBuffered = true;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Margin = new System.Windows.Forms.Padding(2);
+      this.Name = "Form1";
+      this.ShowIcon = false;
+      this.Text = "Form1";
+      this.TopMost = true;
+      this.TransparencyKey = System.Drawing.Color.Black;
+      this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Timer timer;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Panel panel2;
+  }
+}
+
---
+++ z/Form1.cs
@@ -0,0 +1,32 @@
+using NAudio.CoreAudioApi;
+
+namespace SoundView {
+  public partial class Form1 : Form {
+    MMDevice device = null;
+
+    public Form1() {
+      InitializeComponent();
+      MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+      var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+      device = devices.ToArray()[0];
+
+    }
+
+    private void timer1_Tick(object sender, EventArgs e) {
+      if (device != null) {
+        var val = (int)(((device.AudioMeterInformation.PeakValues[0] - device.AudioMeterInformation.PeakValues[1]) * 50) + 50);
+        if (val < 35) {
+          panel2.BackColor = Color.Green;
+        } else {
+          panel2.BackColor = SystemColors.Control;
+        }
+
+        if (val > 65) {
+          panel1.BackColor = Color.Green;
+        } else {
+          panel1.BackColor = SystemColors.Control;
+        }
+      }
+    }
+  }
+}
