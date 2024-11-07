4589987dd284948dc19acf59a3a0606af69bd8dd
id-shared <it.is.a.shared@gmail.com>
2024-11-07 02:16:15 -0800

    changes

--- z/Form1.cs
+++ z/Form1.cs
@@ -9,20 +9,27 @@ namespace SoundView {
       MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
       var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
       device = devices.ToArray()[0];
-
     }
 
     private void timer1_Tick(object sender, EventArgs e) {
       if (device != null) {
-        var val = (int)(((device.AudioMeterInformation.PeakValues[0] - device.AudioMeterInformation.PeakValues[1]) * 50) + 50);
-        if (val < 35) {
-          panel2.BackColor = Color.Green;
+        // Calculate the difference between left and right channels, emphasizing the difference
+        var leftRightDifference = device.AudioMeterInformation.PeakValues[0] - device.AudioMeterInformation.PeakValues[1];
+        
+        // Increase sensitivity by amplifying the difference
+        var val = (int)(((leftRightDifference * 50) + 50)); // Higher multiplier for more sensitivity
+
+        Console.WriteLine(val);
+
+        // Adjust thresholds for more sensitivity
+        if (val < 25) {
+          panel2.BackColor = Color.Red;
         } else {
           panel2.BackColor = SystemColors.Control;
         }
 
-        if (val > 65) {
-          panel1.BackColor = Color.Green;
+        if (val > 75) {
+          panel1.BackColor = Color.Red;
         } else {
           panel1.BackColor = SystemColors.Control;
         }
