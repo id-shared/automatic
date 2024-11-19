3edb26adcf4894c87eb5d71567f719e0f2549017
id-shared <it.is.a.shared@gmail.com>
2024-11-19 08:25:34 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -124,19 +124,19 @@ int to_integer(double e) {
 }
 
 bool move(HANDLE x, double e_y, double e_x, double e_4, double e_3, double e_2, double e_1, bool a) {
-  const double _y = e_2 >= _ ? min(e_4, e_2) : max(-e_4, e_2);
-  const double _x = e_1 >= _ ? min(e_3, e_1) : max(-e_3, e_1);
+  const double y_ = e_2 >= _ ? min(e_4, e_2) : max(-e_4, e_2);
+  const double x_ = e_1 >= _ ? min(e_3, e_1) : max(-e_3, e_1);
 
   if (a) {
-    if (_y >= _) {
-      return Xyloid2::yx(x, to_integer(_y * e_y), to_integer((_x * e_x) / +4));
+    if (y_ >= _) {
+      return Xyloid2::yx(x, to_integer(y_ * e_y), to_integer(x_ * e_x));
     }
     else {
-      return Xyloid2::yx(x, _, to_integer(_x * e_x));
+      return Xyloid2::yx(x, _, to_integer(x_ * e_x));
     }
   }
   else {
-    return Xyloid2::yx(x, to_integer((_y * e_y) / +4), to_integer(_x * e_x));
+    return Xyloid2::yx(x, to_integer(y_ * e_y), to_integer(x_ * e_x));
   }
 };
 
