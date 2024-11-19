5dbfc14546dd2992cedd2bfdbdce90fe1898c0a2
id-shared <it.is.a.shared@gmail.com>
2024-11-19 01:04:56 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -124,10 +124,10 @@ int to_integer(double e) {
 }
 
 bool move(HANDLE x, double e_y, double e_x, double e_4, double e_3, double e_2, double e_1, bool a) {
-  const double y_2 = e_4 >= abs(e_2) ? e_4 / (e_4 / +2) : e_4;
-  const double x_2 = e_3 >= abs(e_1) ? e_3 / (e_4 / +2) : e_3;
-  const double y_ = e_2 >= _ ? min(+y_2, e_2) : max(-y_2, e_2);
-  const double x_ = e_1 >= _ ? min(+x_2, e_1) : max(-x_2, e_1);
+  const double y_2 = (e_4 >= abs(e_2) ? e_4 / +2 / +2 : e_4) / +2;
+  const double x_2 = (e_3 >= abs(e_1) ? e_3 / +2 / +2 : e_3) / +2;
+  const double y_ = e_2 >= _ ? min(y_2, e_2) : max(-y_2, e_2);
+  const double x_ = e_1 >= _ ? min(x_2, e_1) : max(-x_2, e_1);
   const int _y = a ? _ : to_integer(y_ * e_y);
   const int _x = to_integer(x_ * e_x);
   return Xyloid2::yx(x, _y, _x);
