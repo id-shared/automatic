68eb1205f1612b4e49840492d858b73080f7db86
id-shared <it.is.a.shared@gmail.com>
2024-11-20 01:38:28 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -128,7 +128,7 @@ bool move(HANDLE x, double e_y, double e_x, double e_4, double e_3, double e_2, 
   const double x_ = e_1 >= _ ? min(e_3, e_1) : max(-e_3, e_1);
 
   if (a) {
-    const int _x = x_ >= -e_3 && x_ <= e_3 ? +2 : +1;
+    const int _x = +1; // x_ >= -e_3 && x_ <= e_3 ? +2 : +1;
 
     if (y_ >= _) {
       return Xyloid2::yx(x, to_integer((y_ * e_y) / +2), to_integer((x_ * e_x) * _x));
