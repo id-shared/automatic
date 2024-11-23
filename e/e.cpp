f9aaa9f39914b9c5217dd6dd3563e02bff7c0241
id-shared <it.is.a.shared@gmail.com>
2024-11-23 12:50:46 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -66,14 +66,14 @@ int main() {
 
   constexpr UINT VK_D = 0x44;
   constexpr UINT VK_A = 0x41;
-  UINT _z = +4;
+  const UINT frame_rate = +1;
+  UINT _z = _;
   UINT _y = _;
   UINT _x = _;
   UINT _r = _;
   UINT _l = _;
-  UINT _a = _;
 
-  std::function<void()> action2 = [&_a, &_l, &_r, &_x, &_y, &_z, &driver]() {
+  std::function<void()> action2 = [&_l, &_r, &_x, &_y, &_z, &driver]() {
     Parallel::Pool zz(+1000);
     Parallel::Pool zy(+1);
     Parallel::Pool zx(+1);
@@ -82,7 +82,7 @@ int main() {
     const int xx = GetSystemMetrics(SM_CXSCREEN);
 
     const int ey = xy / +16;
-    const int ex = xx / +8;
+    const int ex = xx / +4;
 
     const int cy = ey / +2;
     const int cx = ex / +2;
@@ -90,28 +90,28 @@ int main() {
     const int ay = +2;
     const int ax = +2;
 
-    std::function<bool(int, int, int, int)> work = [&_a, &_l, &_r, &_x, &_y, &ax, &ay, &xx, &xy, &zx, &driver](int e_3, int e_2, int e_1, int e) {
+    std::function<bool(int, int, int, int)> work = [&_l, &_r, &_x, &_y, &_z, &ax, &ay, &xx, &xy, &zx, &driver](int e_3, int e_2, int e_1, int e) {
       const int y_ = e_3 + e_1;
       const int x_ = e_2 + e;
 
       /***/if (_r > _) {
-        x_ == _x && y_ == _y ? _ : move(driver, y_ * ay, x_ * ax, xy / +64., xx / +64., _a > _);
+        x_ == _x && y_ == _y ? _ : move(driver, y_ * ay, x_ * ax, xy / +64., xx / +64., _z > _);
 
         if (_x > _ && abs(e_2) < +4) {
           Xyloid2::e1(driver, true);
           Xyloid2::e1(driver, false);
         }
 
-        _a = _a + 1;
+        _z = _z + 1;
         _y = y_;
         _x = x_;
 
         return true;
       }
       else if (_l > _) {
-        x_ == _x && y_ == _y ? _ : move(driver, y_ * ay * (_a > _ ? _ : +1), x_ * ax * (_a > _ ? +0.5 : +1), xy / +32., xx / +32., _a > _);
+        x_ == _x && y_ == _y ? _ : move(driver, y_ * ay * (_z > _ ? _ : +1), x_ * ax * (_z > _ ? +0.5 : +1), xy / +32., xx / +32., _z > _);
 
-        _a = _a + 1;
+        _z = _z + 1;
         _y = y_;
         _x = x_;
 
@@ -155,7 +155,7 @@ int main() {
       return false;
       };
 
-    std::function<bool(uint8_t*, UINT, UINT, UINT)> each = [&_x, &_y, &_z, &zz, &find](uint8_t* o1, UINT e_2, UINT e_1, UINT e) {
+    std::function<bool(uint8_t*, UINT, UINT, UINT)> each = [&_x, &_y, &zz, &find](uint8_t* o1, UINT e_2, UINT e_1, UINT e) {
       zz.enqueue_task([&find, o1, e_2, e_1, e]() mutable {
         /***/if (find(o1, e_2, e_1, e)) {
           return true;
@@ -168,12 +168,12 @@ int main() {
       return true;
       };
 
-    Capture::screen(each, (xy - ey) / +2, (xx - ex) / +2, ey, ex, _z);
+    Capture::screen(each, (xy - ey) / +2, (xx - ex) / +2, ey, ex, frame_rate);
     };
   std::thread thread2(action2);
 
 
-  std::function<void()> action1 = [&_a, &_l, &_r, &_z, &driver]() {
+  std::function<void()> action1 = [&_l, &_r, &_z, &driver]() {
     Parallel::Pool zy(+1);
     Parallel::Pool zx(+1);
     Parallel::Pool zr(+1);
@@ -183,11 +183,11 @@ int main() {
     const int size = +64;
     int at = +1;
 
-    Event::KeyboardHook hook([&_a, &_l, &_r, &_z, &at, &driver, &zl, &zr, &zy](UINT e, bool a) {
+    Event::KeyboardHook hook([&_l, &_r, &_z, &at, &driver, &zl, &zr, &zy](UINT e, bool a) {
       /***/if (e == VK_OEM_6) {
         if (a) {
           _r = _r + 1;
-          _a = _;
+          _z = _;
 
           zr.enqueue_task([&_r, &driver]() mutable {
             _r > _ ? Xyloid2::e2(driver, true) : _;
@@ -208,12 +208,12 @@ int main() {
       else if (e == VK_OEM_4) {
         if (a) {
           _l = _l + 1;
-          _a = _;
+          _z = _;
 
-          zl.enqueue_task([&_a, &_l, &_z, &at, &driver, &zy]() mutable {
+          zl.enqueue_task([&_l, &_z, &at, &driver, &zy]() mutable {
             UINT e_ = _;
-            while (_l > _ && _a == _ && e_ < +4) {
-              Time::XO(_z);
+            while (_l > _ && _z == _ && e_ < +4) {
+              Time::XO(frame_rate);
               e_ = e_ + 1;
             }
 
@@ -264,6 +264,14 @@ int main() {
             });
 
           return false;
+        }
+      }
+      else if (e == VK_A || e == VK_D) {
+        if (a) {
+          return true;
+        }
+        else {
+          return true;
         }
       }
       else {
