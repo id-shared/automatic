e44658f0fba42ed7b8ef2637a8331e2e74078b8e
id-shared <it.is.a.shared@gmail.com>
2024-11-30 09:00:44 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -170,21 +170,29 @@ int main() {
       _e = _e + 1;
 
       _Z1K.enqueue_task([&_e, &find, o1, e_2, e_1, e]() mutable {
-        /***/if (_e % +3 == _ && find(o1, AA / +1, e_2, e_1 / +8, e, false)) {
-          /**/if (_e % +3 == _ && find(o1, AA / +1, e_2, e_1 / +4, e, true)) {
+        /***/if (_e % +3 == _ && find(o1, AA / +1., e_2, e_1 / +16, e, false)) {
+          /**/if (_e % +3 == _ && find(o1, AA / +1., e_2, e_1 / +8, e, true)) {
             return true;
           }
           else {
             return true;
           }
         }
-        else if (_e % +3 == _ && find(o1, AA / +2, e_2, e_1 / +4, e, true)) {
+        else if (_e % +3 == _ && find(o1, AA / +1., e_2, e_1 / +8, e, false)) {
+          /**/if (_e % +3 == _ && find(o1, AA / +1., e_2, e_1 / +4, e, true)) {
+            return true;
+          }
+          else {
+            return true;
+          }
+        }
+        else if (_e % +3 == _ && find(o1, AA / +2., e_2, e_1 / +4, e, true)) {
           return true;
         }
-        else if (_e % +3 == _ && find(o1, AA / +4, e_2, e_1 / +2, e, true)) {
+        else if (_e % +3 == _ && find(o1, AA / +4., e_2, e_1 / +2, e, true)) {
           return true;
         }
-        else if (_e % +1 == _ && find(o1, AA / +8, e_2, e_1 / +1, e, true)) {
+        else if (_e % +1 == _ && find(o1, AA / +8., e_2, e_1 / +1, e, true)) {
           return true;
         }
         else {
