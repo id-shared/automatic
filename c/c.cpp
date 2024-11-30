499834ff5064fe24c038fff0ff1d2ebaaaa91a0d
id-shared <it.is.a.shared@gmail.com>
2024-11-29 16:15:21 -0800

    Update c.cpp

--- c/c.cpp
+++ c/c.cpp
@@ -24,19 +24,19 @@ int main() {
     int ay = n6 == 255 ? (n5 - n6) - 1 : n5 - n6;
     int ax = n4 == 255 ? (n3 - n4) - 1 : n3 - n4;
     bool a_ = n1 == _;
-    n1 == _ ? false : ax == _ && ay == _ ? false : Xyloid2::yx(driver, ay, ax);
+    a_ == o1[0] ? ax == _ && ay == _ ? false : Xyloid2::yx(driver, ay, ax) : false;
 
     bool a1 = n1 == +1;
-    a1 == o1[1] ? true : Xyloid1::ea(driver, 0x1a, a1);
+    a1 == o1[1] ? false : Xyloid1::ea(driver, 0x1a, a1);
 
     bool a2 = n1 == +4;
-    a2 == o1[2] ? true : Xyloid1::ea(driver, 0x1b, a2);
+    a2 == o1[2] ? false : Xyloid1::ea(driver, 0x1b, a2);
 
     bool a3 = n1 == +8;
-    a3 == o1[3] ? true : Xyloid2::e1(driver, a3);
+    a3 == o1[3] ? false : Xyloid2::e1(driver, a3);
 
     bool a4 = n1 == +16;
-    a4 == o1[4] ? true : Xyloid2::e2(driver, a4);
+    a4 == o1[4] ? false : Xyloid2::e2(driver, a4);
 
     Driver::Byte n7 = o1_1[6];
 
