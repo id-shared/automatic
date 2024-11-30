2936848db2ed6f42ba5e47cbfba67320475b6d4e
id-shared <it.is.a.shared@gmail.com>
2024-11-30 03:19:22 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -154,21 +154,24 @@ int main() {
       Axis axis = detect(o1, ay, ax, e_2, e_1, e);
 
       if (axis.is) {
-        return a && task(axis.y, axis.x, e_3);
+        if (a) {
+          return task(axis.y, axis.x, e_3);
+        }
+        else {
+          return true;
+        }
       }
       else {
         return false;
       }
       };
 
-    std::function<bool(uint8_t*, UINT, UINT, UINT)> each = [&_e, &_X, &_Y, &_Z1K, &ax, &ay, &find](uint8_t* o1, UINT e_2, UINT e_1, UINT e) mutable {
+    std::function<bool(uint8_t*, UINT, UINT, UINT)> each = [&_e, &_X, &_Y, &_Z1K, &find](uint8_t* o1, UINT e_2, UINT e_1, UINT e) mutable {
       _e = _e + 1;
 
-      _Z1K.enqueue_task([&_e, &ax, &ay, &find, o1, e_2, e_1, e]() mutable {
-        Axis axis = detect(o1, ay, ax, e_2, e_1 / +8, e);
-
-        /***/if (axis.is) {
-          if (_e % +2 == _ && find(o1, AA / +1, e_2, e_1 / +4, e, true)) {
+      _Z1K.enqueue_task([&_e, &find, o1, e_2, e_1, e]() mutable {
+        /***/if (_e % +2 == _ && find(o1, AA / +1, e_2, e_1 / +8, e, false)) {
+          if (_e % +4 == _ && find(o1, AA / +1, e_2, e_1 / +4, e, true)) {
             return true;
           }
           else {
