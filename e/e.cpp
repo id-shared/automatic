567f03da02243b5601684890ca038ca1483343de
id-shared <it.is.a.shared@gmail.com>
2024-12-01 05:04:18 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -168,24 +168,21 @@ int main() {
 
     std::function<bool(uint8_t*, UINT, UINT, UINT)> each = [&_e, &_X, &_Y, &_Z1K, &find](uint8_t* o1, UINT e_2, UINT e_1, UINT e) mutable {
       _Z1K.enqueue_task([&_e, &find, o1, e_2, e_1, e]() mutable {
-        /***/if (_e == _ || _e % +3 == _) {
-          _e = _e + 1;
+        _e = _e + 1;
 
+        /***/if (_e % +4 == _) {
           /***/if (find(o1, AA / +1., e_2, e_1 / +16, e, false)) {
-            /***/if (find(o1, AA / +1., e_2, e_1 / +4, e, true)) {
+            /***/if (find(o1, AA / +1., e_2, e_1 / +2, e, true)) {
               return true;
             }
             else {
               return true;
             }
           }
-          else if (find(o1, AA / +1., e_2, e_1 / +4, e, true)) {
+          else if (find(o1, AA / +1., e_2, e_1 / +2, e, true)) {
             return true;
           }
-          else if (find(o1, AA / +2., e_2, e_1 / +2, e, true)) {
-            return true;
-          }
-          else if (find(o1, AA / +4., e_2, e_1 / +1, e, true)) {
+          else if (find(o1, AA / +2., e_2, e_1 / +1, e, true)) {
             return true;
           }
           else {
