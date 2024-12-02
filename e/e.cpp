6d40ae56e326289c5b8fb777307b68774566428a
id-shared <it.is.a.shared@gmail.com>
2024-12-02 11:43:17 -0800

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -139,7 +139,7 @@ int main() {
     const int ex = GetSystemMetrics(SM_CXSCREEN);
 
     const int cy = ey / +16;
-    const int cx = ex / +4;
+    const int cx = ex / +8;
 
     const int ay = cy / +2;
     const int ax = cx / +2;
